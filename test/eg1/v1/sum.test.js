7d06840d066793cf1d2813be7fa1eb288285547c
xubaorui <xubaorui123@163.com>
2022-03-22 17:13:37 +0800

    feature:抽离statement方法

---
+++ test/eg1/v1/sum.test.js
@@ -0,0 +1,36 @@
+const statement = require('../../../src/eg1/v1/statement')
+
+test("statement", () => {
+    var invoices =
+        {
+            "customer": "BigCo",
+            "performances": [
+                {
+                    "playID": "hamlet",
+                    "audience": 55
+                },
+                {
+                    "playID": "as-like",
+                    "audience": 35
+                },
+                {
+                    "playID": "othello",
+                    "audience": 40
+                }
+            ]
+        }
+    var plays_json =
+        {
+            "hamlet": {"name": "Hamlet", "type": "tragedy"},
+            "as-like": {"name": "As You Like It", "type": "comedy"},
+            "othello": {"name": "Othello", "type": "tragedy"}
+        }
+
+    function sta() {
+        let result = statement(invoices, plays_json);
+        console.log(result);
+        return result;
+    }
+
+    expect(sta()).not.toEqual("");
+});
